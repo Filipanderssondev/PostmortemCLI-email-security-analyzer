942f6583e1c8de2f5eb163dc8da7eb837471c442
filipanderssondev <andersson.filip98@gmail.com>
2026-04-22 16:59:26 +0200

    Translate to english, refined

--- src/smtp_reciever.py
+++ src/smtp_reciever.py
@@ -4,35 +4,30 @@ import asyncio
 from aiosmtpd.controller import Controller
 from aiosmtpd.handlers import AsyncMessage
 
-
-class PostMortemHandler(AsyncMessage):
+class PostMortemMessageHandler(AsyncMessage):
     async def handle_message(self, message):
-        # "async" = den här funktionen kan pausa och vänta utan att blockera allt annat
-        # "message" = hela e-postmeddelandet som ett Python email.message-objekt
-
-        print("=== NYTT MAIL MOTTAGET ===")
-        print(f"Från:     {message['From']}")       # Hämtar From-headern
-        print(f"Till:     {message['To']}")         # Hämtar To-headern
-        print(f"Ämne:     {message['Subject']}")    # Hämtar Subject-headern
+        print("=== NEW MAIL RECIEVED ===")
+        print(f"From:     {message['From']}")
+        print(f"To:     {message['To']}")
+        print(f"Subject:     {message['Subject']}")
         print("==========================")
 
-        # Här anropar vi senare parser.py
+        # Call parser.py here
         # parsed = parse_email(message)
         # result = analyze(parsed)
         # send_report(result)
 
 async def main():
-
-    handler = PostMortemHandler()        
+    handler = PostMortemMessageHandler()        
     controller = Controller(
         handler,
         hostname="0.0.0.0",               
     )
 
     controller.start()
-    print("📬 Post-Mortem SMTP-mottagare körs på port 1025...")
-    print("Skicka mail till: scan@localhost")
-    print("Tryck Ctrl+C för att stoppa.")
+    print("📬 Post-Mortem SMTP-reciever running on port 1025...")
+    print("Send mail to: scan@localhost")
+    print("Press Ctrl+C to stop.")
 
     try:
         await asyncio.sleep(float("inf")) 
@@ -41,7 +36,7 @@ async def main():
 
     finally:
         controller.stop()
-        print("Server stoppad.")
+        print("Server stopped.")
 
 if __name__ == "__main__":
     asyncio.run(main()) 
--- tests/test_reciever.py
+++ tests/test_reciever.py
@@ -5,14 +5,14 @@ import smtplib
 from email.mime.text import MIMEText
 
 def send_test_email():
-    msg = MIMEText('Hej från Post-Mortem!')
+    msg = MIMEText('Hello from the test mail!')
     msg['Subject'] = 'Testmail'
     msg['From'] = 'filip@test.com'
     msg['To'] = 'scan@localhost'
 
     with smtplib.SMTP('localhost', 1025) as s:
         s.sendmail('filip@test.com', ['scan@localhost'], msg.as_string())
-        print('✅ Mail skickat!')
+        print('✅ Mail sent!')
 
 if __name__ == "__main__":
     send_test_email()
